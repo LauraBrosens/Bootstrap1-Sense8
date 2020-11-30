5025992b829fb69e5e2b1c6e243e69f5b92337e6
Laura Brosens Barros <brosensbarros.laura@gmail.com>
2020-11-30 16:45:49 -0300

    versão 0.5 do meu site

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+let sensate = document.querySelectorAll(".sensate")
+let sensateText = document.querySelectorAll(".sensateText")
+
+function sensateVisivel(a) {
+    sensateText[a].style = "visibility: hidden;"
+}
+
+function sensateInvisivel(a){
+    sensateText[a].style = "visibility: visible;"
+}
